39689fac9f61b7dceedfa7fd6edb5cdb61a22021
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-13 16:58:14 +0100

    Before API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7A7DFE;
@@ -23,7 +24,7 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
 
     return (
         <form>
