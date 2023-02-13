8ffaf7ae0235a9b1123dc08e5396765f40864af4
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-13 19:18:04 +0100

    both Selects DONE

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from '../data/monedas';
@@ -24,11 +25,37 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
+    const [ criptos, setCriptos ] = useState([]);
+
     const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+    const [ criptomoneda, SelectCriptomoneda ] = useSelectMonedas('Elige tu Cripto', criptos);
+
+    useEffect( () => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                };
+
+                return objeto;
+            });
+
+            setCriptos( arrayCriptos );
+        };
+
+        consultarAPI();
+    }, []);
 
     return (
         <form>
             <SelectMonedas />
+            <SelectCriptomoneda />
 
             <InputSubmit 
                 type='submit' 
