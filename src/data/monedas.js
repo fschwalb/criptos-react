280906b79996281fef52cac8861c643e46fb7363
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-17 18:39:44 +0100

    Spinner and Styles

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,6 +1,5 @@
 const monedas = [
     { id: 'USD', nombre: 'Dolar USA' },
-    { id: 'ARG', nombre: 'Peso Argentino' },
     { id: 'COL', nombre: 'Peso Colombiano' },
     { id: 'EUR', nombre: 'Euro' },
     { id: 'GBP', nombre: 'Libra' }
